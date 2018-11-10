4317e5c8ae0aa2acaec3fba5b22421d34fb87c2c
taojian <taojian86@yahoo.com>
2018-11-10 17:08:59 +0800

    创建一个函数型的component

--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
+// 最好导入的时候，最好使用首字母大写，因为jsx中所有的components用小写开头
 
 class App extends Component {
+
     render() {
-        return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'hello, this is rendered by React.createElement'));
+        return (
+            <div className="App">
+                <h1>Alex</h1>
+                <p>Alex is now here </p>
+                <Person/>
+                {/* 导入person模块*/}
+            </div>
+        //    return中只能有一个根 元素
+        )
     }
+
+
+    // render() {
+    //     return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'hello, this is rendered by React.createElement'));
+    // }
 }
 
 export default App;
---
+++ react-learning/src/Person/Person.js
@@ -0,0 +1,12 @@
+// 主要的作用是返回一个jsx语法的component, component是一个函数，返回some jsx
+import React from 'react';
+
+
+
+const person = () => {
+    return <p> I am a person named alex！</p>
+
+};
+
+
+export default person;
