4c4ea89c0d5dae6b7ea26beb54e7110658e629f9
taojian <taojian86@yahoo.com>
2018-11-10 16:01:11 +0800

    JSX语法的一些限制

--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    // 两种限制：
+    // 1. class要写成className, 因为class是关键预留字
+    // 2. JSX里面只能写一个根element, 不能并列写两个或者多个element
+
     render() {
         return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'hello, this is rendered by React.createElement'));
     }
